49ff74b2c2c9370d1d2b3fcd7bc7def087b83558
priyankadevops <priyankahiwale94@gmail.com>
2025-09-25 20:29:13 +0000

    1st Assignment

---
+++ shellscript/backup_critical_data.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+#This script will backup critical data
+#
+#Author: Priyanka Hiwale
+#
+#Usage: ./backup_critical_data.sh
+#Notes:
+#  - Make sure that script have executable permission.
+#  - You can schedule it via cron for automated backups.
+#
+
+# Exit on error
+set -e
+
+SOURCE=(
+   "/home/priyanka/important"
+   "/tmp/important"
+)
+
+BACKUP_DIR="/home/priyanka/backups"
+DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+BACKUP_NAME="critical_backup_$DATE.tgz"
+LOG_FILE="/home/priyanka/backups/backup_script.log"
+
+# Create backup directory if it doesn't exist
+mkdir -p $BACKUP_DIR
+
+# Logging Starts
+echo "[$(date)] Starting backup | tee -a $LOG_FILE"
+
+# Create Backup
+tar -czf $BACKUP_DIR/$BACKUP_NAME ${SOURCE[@]}
+
+# Logging finish
+echo "[$(date)] Backup Completed: $BACKUP_DIR/$BACKUP_NAME" | tee -a $LOG_FILE
